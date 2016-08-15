50cdc88b4cba6c53e5df4032620b6117260930ad
iMac <imac@iMac-iMac.local>
2016-08-15 18:19:06 +0300

    first commit

---
+++ Lesson1/Lesson_1(homework)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lesson_1(homework)
+//
+//  Created by iMac on 15.08.16.
+//  Copyright © 2016 Kostia Lelikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson1/Lesson_1(homework)/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson_1(homework)
+//
+//  Created by iMac on 15.08.16.
+//  Copyright © 2016 Kostia Lelikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
